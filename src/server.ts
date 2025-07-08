25d4cf17001b8558abe8cc8225ade6edfed5b5aa
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-07-08 23:27:18 +0600

    Refactor server.ts to use environment variable for DB_URI and add type annotation for server variable

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,20 @@
 import mongoose from "mongoose";
+import { Server } from "http";
 import app from "./app";
 
-let server;
+let server: Server;
 const PORT = 3000;
 
 async function main() {
+  const DB_URI = process.env.DB_URI as string;
   try {
-    await mongoose.connect(
-      "mongodb+srv://MongoDB:MongoDB@cluster0.x7pm4nr.mongodb.net/testDB?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    await mongoose.connect(DB_URI);
     console.log("Connected to MongoDB");
-    app.listen(PORT, () => {
+    server = app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
   } catch (error) {
     console.error("Error starting the server:", error);
   }
 }
-main()
+main();
