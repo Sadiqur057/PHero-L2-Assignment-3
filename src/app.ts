58e441943e06b9f0c28095ffc4de2279c1b2db45
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-07-08 23:26:57 +0600

    Implement API routes for books and borrowing, and add 404 error handling

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,30 @@
 import express, { Application, Request, Response } from "express";
+import { bookRoutes } from "./app/controllers/book.controller";
+import { borrowRoutes } from "./app/controllers/borrow.controller";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app: Application = express();
 app.use(express.json());
 
+app.use("/api/books", bookRoutes);
+
+app.use("/api/borrow", borrowRoutes);
+
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
     message: "Welcome to the Library Management API",
+  });
+});
+
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: "Resource not found",
+    error: {
+      name: "NotFoundError",
+      message: `Cannot ${req.method} ${req.originalUrl}`,
+    },
   });
 });
 
