0929d454567e1d8de842d58cdb043d892a6f6de0
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-07-08 23:29:26 +0600

    Add IBook interface and BookInstanceMethods for book management

---
+++ src/app/interfaces/book.interface.ts
@@ -0,0 +1,18 @@
+export interface IBook {
+  title: string;
+  author: string;
+  genre:
+    | "FICTION"
+    | "NON_FICTION"
+    | "SCIENCE"
+    | "HISTORY"
+    | "BIOGRAPHY"
+    | "FANTASY";
+  isbn: number;
+  description?: string;
+  copies: number;
+  available?: boolean;
+}
+export interface BookInstanceMethods {
+  updateAvailability(): Promise<IBook>;
+}
